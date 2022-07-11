2d328eb7cc4a825223e5b9f8ca095cec1633ef1d
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-11 14:56:20 -0400

    Fetch data from API

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,6 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 const Test = () => {
+  const [characters, setCharacters] = useState(null);
+
+  // Run only once
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  // Fetch data from API
+  const getCharacters = () => {
+    fetch('https://rickandmortyapi.com/api/character', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        // Setting data obtained from API to the characters state
+        setCharacters(data);
+      })
+      // Handle errors
+      .catch(error => {
+        console.log(error);
+      });
+  };
   return <h1>Test</h1>;
 };
 
